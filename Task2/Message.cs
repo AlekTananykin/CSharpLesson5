64bcd7bfe715776fb2378f370d6282ca3b384783
AlekTananykin <tananykin@mail.ru>
2020-11-09 00:24:54 +0300

    Task 2

---
+++ Task2/Message.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    static class Message
+    {
+        private static string RemovePunctuation(string message)
+        {
+            return Regex.Replace(message, @"[ ,.!?(){}\[\]-]", " ");
+        }
+
+        private static string[] ConvertToWordArray(string message)
+        {
+            message = RemovePunctuation(message);
+            return message.Split(new char[] { ' ' });
+        }
+
+        public static string MaxWordSizeMessage(string message, int maxWordSize)
+        {
+            string[] wordArray = ConvertToWordArray(message);
+
+            StringBuilder resultMessage = new StringBuilder();
+            for (int i = 0; i < wordArray.Length; ++i)
+            {
+                if (0 == wordArray[i].Length)
+                    continue;
+
+                if (wordArray[i].Length <= maxWordSize)
+                {
+                    if (0 == resultMessage.Length)
+                        resultMessage.AppendFormat(wordArray[i]);
+                    else
+                        resultMessage.AppendFormat(", {0}", wordArray[i]);
+                }
+            }
+
+            return resultMessage.ToString();
+        }
+
+        public static string RemoveWordsWithEnd(string message, char symbol)
+        {
+            StringBuilder pattern = new StringBuilder();
+            pattern.AppendFormat(@"\b[a-zA-Z0-9]*{0}\b", symbol);
+
+            return Regex.Replace(message, pattern.ToString(), String.Empty);
+        }
+
+        public static string MaxSizeWord(string message)
+        {
+            string[] wordArray = ConvertToWordArray(message);
+
+            if (0 == wordArray.Length)
+                return string.Empty;
+
+            string maxSizeWord = wordArray[0];
+            for (int i = 0; i < wordArray.Length; ++i)
+                if (maxSizeWord.Length < wordArray[i].Length)
+                    maxSizeWord = wordArray[i];
+
+            return maxSizeWord;
+        }
+
+        public static string GetMaxSizeWords(string message)
+        {
+            string[] wordArray = ConvertToWordArray(message);
+
+            if (0 == wordArray.Length)
+                return string.Empty;
+
+            int maxSize = wordArray[0].Length;
+            
+            for (int i = 0; i < wordArray.Length; ++i)
+                if (maxSize < wordArray[i].Length)
+                    maxSize = wordArray[i].Length;
+
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < wordArray.Length; ++i)
+                if (maxSize == wordArray[i].Length)
+                    result.AppendFormat("{0} ", wordArray[i]);
+
+            return result.ToString();
+        }
+
+        public static void FrequencyAnalyze(string messageText, 
+            string words, IDictionary<string, int> dictionary)
+        {
+            string[] wordArray = ConvertToWordArray(words);
+            for (int i = 0; i < wordArray.Length; ++i)
+            {
+                string targetWord = wordArray[i];
+                if (0 == targetWord.Length)
+                    continue;
+
+                int wordsCount = 0;
+                int startIndex = 0;
+                while (true)
+                {
+                    startIndex = messageText.IndexOf(targetWord, startIndex);
+                    if (-1 == startIndex)
+                        break;
+                    ++startIndex;
+                    ++wordsCount;
+                }
+                dictionary[targetWord] = wordsCount;
+            }
+        }
+
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Тананыкин
+ * 
+ * Разработать статический класс Message, содержащий следующие статические 
+ * методы для обработки текста:
+ * а) Вывести только те слова сообщения,  которые содержат не более n букв.
+ * б) Удалить из сообщения все слова, которые заканчиваются на заданный 
+ * символ.
+ * в) Найти самое длинное слово сообщения.
+ * г) Сформировать строку с помощью StringBuilder из самых длинных слов 
+ * сообщения.
+ * д) ***Создать метод, который производит частотный анализ текста. В качестве 
+ * параметра в него передается массив слов и текст, в качестве результата метод 
+ * возвращает сколько раз каждое из слов массива входит в этот текст. 
+ * Здесь требуется использовать класс Dictionary.
+ */
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сообщение:");
+
+            string messageText = Console.ReadLine();
+
+            RemoveSizeWords(messageText);
+            RemoveSymbolWords(messageText);
+
+            Console.WriteLine("в)Самое длинное слово сообщения: {0}\n", 
+                Message.MaxSizeWord(messageText));
+
+            Console.WriteLine("г)Самые длинные слова: {0}", 
+                Message.GetMaxSizeWords(messageText));
+
+            PrintFrequencyAnalyse();
+
+            Console.WriteLine("\nВведите любой символ для завершения программы. ");
+            Console.ReadKey();
+        }
+
+        private static void RemoveSizeWords(string messageText)
+        {
+            Console.WriteLine();
+            Console.Write("Введите максимальную длину слова: ");
+            int maxWordSize = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("а)Сообщение из слов длиной не более {0} " +
+                "символов: {1}",
+                maxWordSize, Message.MaxWordSizeMessage(messageText, maxWordSize));
+        }
+
+        private static void RemoveSymbolWords(string messageText)
+        {
+            Console.WriteLine();
+            Console.WriteLine("\nб)Удаление из сообщения всех слов заканчивающихся " +
+               "на заданный символ. ");
+            Console.Write("Введите символ:");
+            char ch = Console.ReadKey().KeyChar;
+            Console.WriteLine("\nПолученное сообщение:");
+            Console.WriteLine(Message.RemoveWordsWithEnd(messageText, ch));
+        }
+
+        private static void PrintFrequencyAnalyse()
+        {
+            Console.WriteLine();
+            Console.WriteLine("\nд) Частотный анализ текста. ");
+            Console.WriteLine("Введите текст");
+            string messageText = Console.ReadLine();
+            Console.WriteLine("Введите слова");
+            string words = Console.ReadLine();
+
+            IDictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            Message.FrequencyAnalyze(messageText, words, dictionary);
+            foreach (KeyValuePair<string, int> kvp in dictionary)
+                Console.WriteLine("{0} встречается {1} раз", kvp.Key, kvp.Value);
+        }
+
+    }
+}
